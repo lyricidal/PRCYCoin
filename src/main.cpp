9c53500af93df50263ad9a90745ea9806c74577d
lyricidal <lyricidal@gmail.com>
2020-12-15 18:22:11 -0500

    Bypass bad blocks

--- src/main.cpp
+++ src/main.cpp
@@ -2918,7 +2918,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
         if (!CheckNumberOfAuditedPoSBlocks(block, pindex)) {
             return state.DoS(100, error("ConnectBlock(): A PoA block should audit at least 59 PoS blocks and no more than 120 PoS blocks"));
         }
-        if (!CheckPoABlockNotContainingPoABlockInfo(block)) {
+        if (!CheckPoABlockNotContainingPoABlockInfo(block, pindex)) {
             return state.DoS(100, error("ConnectBlock(): A PoA block should not audit any existing PoA blocks"));
         }
         if (!CheckPoABlockRewardAmount(block, pindex)) {
