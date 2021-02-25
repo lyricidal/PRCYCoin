afa95ea4450f27137a12eb3f87bc32554b99f757
PRCYDev <prcy_dev@protonmail.com>
2021-02-25 14:45:03 -0500

    Modify Staking status on init

--- src/init.cpp
+++ src/init.cpp
@@ -1930,6 +1930,9 @@ bool AppInit2(bool isDaemon)
 
 #ifdef ENABLE_WALLET
     bool storedStakingStatus = false;
+    bool Autoconsolidate = GetBoolArg("-autoconsolidate", false);
+    bool StakingStatus = GetBoolArg("-staking", false);
+
     if (pwalletMain) {
         // Add wallet transactions that aren't already in a block to mapTransactions
         pwalletMain->ReacceptWalletTransactions();
@@ -1940,19 +1943,21 @@ bool AppInit2(bool isDaemon)
         LogPrintf("nDefaultConsolidateTime = %ss\n", nDefaultConsolidateTime); //temp excessive log to check value changes
 
         storedStakingStatus = pwalletMain->ReadStakingStatus();
-        if (GetBoolArg("-staking", true) || (storedStakingStatus == true)) {
+        if (StakingStatus || storedStakingStatus) {
             fGeneratePrcycoins = true;
             LogPrintf("Starting staking\n");
             threadGroup.create_thread(boost::bind(&TraceThread<void (*)()>, "stakemint", &ThreadStakeMinter));
             // stakingMode should be STOPPED on first launch or keep previous setting when available
             // This changes that setting only if staking is on
-            if (GetBoolArg("-autoconsolidate", true)) {
+            if (Autoconsolidate) {
                 LogPrintf("Autoconsolidate is enabled and we are setting StakingMode::STAKING_WITH_CONSOLIDATION now\n");
                 pwalletMain->stakingMode = StakingMode::STAKING_WITH_CONSOLIDATION;
             } else {
                 pwalletMain->stakingMode = StakingMode::STAKING_WITHOUT_CONSOLIDATION;
                 LogPrintf("Autoconsolidate is disabled\n");
             }
+        } else {
+            LogPrintf("Staking is disabled\n");
         }
         //read decoy confirmation min
         pwalletMain->DecoyConfirmationMinimum = GetArg("-decoyconfirm", 15);
--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -53,7 +53,7 @@ bool fSendFreeTransactions = false;
 bool fPayAtLeastCustomFee = true;
 int64_t nStartupTime = GetTime();
 int64_t nReserveBalance = 0;
-int64_t nDefaultConsolidateTime = 0;
+int64_t nDefaultConsolidateTime = GetArg("-autoconsolidatetime", 300);
 
 #include "uint256.h"
 
@@ -5335,7 +5335,6 @@ bool CWallet::CreateSweepingTransaction(CAmount target, CAmount threshold, uint3
 
 void CWallet::AutoCombineDust()
 {
-    nDefaultConsolidateTime = GetArg("-autoconsolidatetime", 300);
     // QT wallet is always locked at startup, return immediately
     if (IsLocked()) return;
     // Chain is not synced, return
