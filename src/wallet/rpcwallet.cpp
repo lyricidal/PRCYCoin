f75e51a48007fa2d170d685f5e2856ebbc9ed722
PRCYDev <prcy_dev@protonmail.com>
2021-02-25 16:25:22 -0500

    disabled autocombinedust completely

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2153,6 +2153,7 @@ UniValue getstakesplitthreshold(const UniValue& params, bool fHelp)
 
 UniValue autocombinedust(const UniValue& params, bool fHelp)
 {
+    if(pwalletMain->fCombineDust){
     bool fEnable;
     if (params.size() >= 1)
         fEnable = params[0].get_bool();
@@ -2188,6 +2189,10 @@ UniValue autocombinedust(const UniValue& params, bool fHelp)
     result.push_back(Pair("autocombinedust", params[0].get_bool()));
     result.push_back(Pair("amount", int(pwalletMain->nAutoCombineThreshold)));
     return result;
+    } else {
+        throw runtime_error(
+        "autocombinedust is disabled in your prcycoin.conf");
+    }
 }
 
 UniValue printMultiSend()
