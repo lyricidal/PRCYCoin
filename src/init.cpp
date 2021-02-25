499f8ff96eae98cdc81de52d903cecc6872faf09
PRCYDev <prcy_dev@protonmail.com>
2021-02-25 17:37:19 -0500

    7 day CLTV activation

--- src/init.cpp
+++ src/init.cpp
@@ -1427,12 +1427,6 @@ bool AppInit2(bool isDaemon)
                 pcoinsdbview = new CCoinsViewDB(nCoinDBCache, false, fReindex);
                 pcoinscatcher = new CCoinsViewErrorCatcher(pcoinsdbview);
                 pcoinsTip = new CCoinsViewCache(pcoinscatcher);
-				
-                bool fIsActiveCLTV;
-                if (!pblocktree->ReadFlag("CLTVHasMajority", fIsActiveCLTV))
-                    fCLTVHasMajority = false;
-                else
-                    fCLTVHasMajority = fIsActiveCLTV;
 
                 if (fReindex)
                     pblocktree->WriteReindexing(true);
