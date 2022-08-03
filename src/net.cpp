e55a709216a8fecaa9c2714091f12deb2fbe3e6b
lyricidal <lyricidal@gmail.com>
2022-08-03 12:42:35 -0400

    CNodeRef copy constructor and assignment operator

--- src/net.cpp
+++ src/net.cpp
@@ -791,6 +791,23 @@ public:
 
     CNode& operator *() const {return *_pnode;};
     CNode* operator ->() const {return _pnode;};
+
+
+    CNodeRef& operator =(const CNodeRef& other)
+    {
+        if (this != &other) {
+            _pnode->Release();
+            _pnode = other._pnode;
+            _pnode->AddRef();
+        }
+        return *this;
+    }
+
+    CNodeRef(const CNodeRef& other):
+        _pnode(other._pnode)
+    {
+        _pnode->AddRef();
+    }
 private:
     CNode *_pnode;
 };
