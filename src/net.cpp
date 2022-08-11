9c47989075578bdec1b66be448af884fdd263005
Sherwin <sherwinkooij@gmail.com>
2022-08-11 20:32:15 +0200

    net: move nLastNodeId to CConnman

--- src/net.cpp
+++ src/net.cpp
@@ -88,9 +88,6 @@ std::deque<std::pair<int64_t, CInv> > vRelayExpiration;
 RecursiveMutex cs_mapRelay;
 limitedmap<CInv, int64_t> mapAlreadyAskedFor(MAX_INV_SZ);
 
-NodeId nLastNodeId = 0;
-RecursiveMutex cs_nLastNodeId;
-
 static CSemaphore *semOutbound = NULL;
 boost::condition_variable messageHandlerCondition;
 
@@ -372,7 +369,7 @@ CNode* CConnman::ConnectNode(CAddress addrConnect, const char* pszDest, bool obf
         addrman.Attempt(addrConnect, fCountFailure);
 
         // Add node
-        CNode *pnode = new CNode(hSocket, addrConnect, pszDest ? pszDest : "", false);
+        CNode* pnode = new CNode(GetNewNodeId(), hSocket, addrConnect, pszDest ? pszDest : "", false);
         GetNodeSignals().InitializeNode(pnode->GetId(), pnode);
         pnode->AddRef();
 
@@ -968,7 +965,7 @@ void CConnman::AcceptConnection(const ListenSocket& hListenSocket) {
         }
     }
 
-    CNode* pnode = new CNode(hSocket, addr, "", true);
+    CNode* pnode = new CNode(GetNewNodeId(), hSocket, addr, "", true);
     GetNodeSignals().InitializeNode(pnode->GetId(), pnode);
     pnode->AddRef();
     pnode->fWhitelisted = whitelisted;
@@ -1874,6 +1871,7 @@ CConnman::CConnman()
 {
     setBannedIsDirty = false;
     fAddressesInitialized = false;
+    nLastNodeId = 0;
 }
 
 bool StartNode(CConnman& connman, boost::thread_group& threadGroup, CScheduler& scheduler, std::string& strNodeError)
@@ -1885,8 +1883,13 @@ bool StartNode(CConnman& connman, boost::thread_group& threadGroup, CScheduler& 
     return ret;
 }
 
-bool CConnman::Start(boost::thread_group& threadGroup, CScheduler& scheduler, std::string& strNodeError)
+NodeId CConnman::GetNewNodeId()
 {
+    return nLastNodeId.fetch_add(1, std::memory_order_relaxed);
+}
+
+bool CConnman::Start(boost::thread_group& threadGroup, CScheduler& scheduler, std::string& strNodeError)
+{   
     uiInterface.InitMessage(_("Loading addresses..."));
     // Load addresses from peers.dat
     int64_t nStart = GetTimeMillis();
@@ -1933,7 +1936,7 @@ bool CConnman::Start(boost::thread_group& threadGroup, CScheduler& scheduler, st
     if (pnodeLocalHost == NULL) {
         CNetAddr local;
         LookupHost("127.0.0.1", local, false);
-        pnodeLocalHost = new CNode(INVALID_SOCKET, CAddress(CService(local, 0), nLocalServices));
+        pnodeLocalHost = new CNode(GetNewNodeId(), INVALID_SOCKET, CAddress(CService(local, 0), nLocalServices));
         GetNodeSignals().InitializeNode(pnodeLocalHost->GetId(), pnodeLocalHost);
     }
 
@@ -2285,7 +2288,7 @@ unsigned int ReceiveFloodSize() { return 1000 * GetArg("-maxreceivebuffer", 5 * 
 
 unsigned int SendBufferSize() { return 1000 * GetArg("-maxsendbuffer", 1 * 1000); }
 
-CNode::CNode(SOCKET hSocketIn, CAddress addrIn, std::string addrNameIn, bool fInboundIn) :
+CNode::CNode(NodeId idIn, SOCKET hSocketIn, const CAddress& addrIn, const std::string& addrNameIn, bool fInboundIn) :
     ssSend(SER_NETWORK, INIT_PROTO_VERSION),
     addrKnown(5000, 0.001),
     filterInventoryKnown(50000, 0.000001)
@@ -2330,10 +2333,7 @@ CNode::CNode(SOCKET hSocketIn, CAddress addrIn, std::string addrNameIn, bool fIn
     nMinPingUsecTime = std::numeric_limits<int64_t>::max();
     fObfuScationMaster = false;
 
-    {
-        LOCK(cs_nLastNodeId);
-        id = nLastNodeId++;
-    }
+    id = idIn;
 
     if (fLogIPs)
         LogPrint(BCLog::NET, "Added connection to %s peer=%d\n", addrName, id);
--- src/net.h
+++ src/net.h
@@ -193,6 +193,9 @@ private:
     bool IsWhitelistedRange(const CNetAddr &addr);
 
     void DeleteNode(CNode* pnode);
+
+    NodeId GetNewNodeId();
+
     //!check is the banlist has unwritten changes
     bool BannedSetIsDirty();
     //!set the "dirty" flag for the banlist
@@ -221,6 +224,7 @@ private:
     RecursiveMutex cs_vAddedNodes;
     std::vector<CNode*> vNodes;
     mutable RecursiveMutex cs_vNodes;
+    std::atomic<NodeId> nLastNodeId;
 };
 extern std::unique_ptr<CConnman> g_connman;
 void MapPort(bool fUseUPnP);
@@ -297,9 +301,6 @@ extern std::map<CInv, CDataStream> mapRelay;
 extern std::deque<std::pair<int64_t, CInv> > vRelayExpiration;
 extern RecursiveMutex cs_mapRelay;
 extern limitedmap<CInv, int64_t> mapAlreadyAskedFor;
-
-extern NodeId nLastNodeId;
-extern RecursiveMutex cs_nLastNodeId;
 
 /** Subversion as sent to the P2P network in `version` messages */
 extern std::string strSubVersion;
@@ -474,7 +475,7 @@ public:
     // Whether a ping is requested.
     bool fPingQueued;
 
-    CNode(SOCKET hSocketIn, CAddress addrIn, std::string addrNameIn = "", bool fInboundIn = false);
+    CNode(NodeId id, SOCKET hSocketIn, const CAddress& addrIn, const std::string& addrNameIn = "", bool fInboundIn = false);
     ~CNode();
 
 private:
