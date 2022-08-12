ede8e120110026a1bf0894369f374a3376a4a50b
Sherwin <sherwinkooij@gmail.com>
2022-08-12 18:00:54 +0200

    net: Pass best block known height into CConnman CConnman then passes the current best height into CNode at creation time.
    This way CConnman/CNode have no dependency on main for height, and the signals
    only move in one direction.
    
    This also helps to prevent identity leakage a tiny bit. Before this change, an
    attacker could theoretically make 2 connections on different interfaces. They
    would connect fully on one, and only establish the initial connection on the
    other. Once they receive a new block, they would relay it to your first
    connection, and immediately commence the version handshake on the second. Since
    the new block height is reflected immediately, they could attempt to learn
    whether the two connections were correlated.
    
    This is, of course, incredibly unlikely to work due to the small timings
    involved and receipt from other senders. But it doesn't hurt to lock-in
    nBestHeight at the time of connection, rather than letting the remote choose
    the time.

--- src/main.cpp
+++ src/main.cpp
@@ -1017,7 +1017,6 @@ bool GetNodeStateStats(NodeId nodeid, CNodeStateStats& stats)
 
 void RegisterNodeSignals(CNodeSignals& nodeSignals)
 {
-    nodeSignals.GetHeight.connect(&GetHeight);
     nodeSignals.ProcessMessages.connect(&ProcessMessages);
     nodeSignals.SendMessages.connect(&SendMessages);
     nodeSignals.InitializeNode.connect(&InitializeNode);
@@ -1026,7 +1025,6 @@ void RegisterNodeSignals(CNodeSignals& nodeSignals)
 
 void UnregisterNodeSignals(CNodeSignals& nodeSignals)
 {
-    nodeSignals.GetHeight.disconnect(&GetHeight);
     nodeSignals.ProcessMessages.disconnect(&ProcessMessages);
     nodeSignals.SendMessages.disconnect(&SendMessages);
     nodeSignals.InitializeNode.disconnect(&InitializeNode);
@@ -3797,6 +3795,7 @@ bool ActivateBestChain(CValidationState& state, const CBlock* pblock, bool fAlre
 
         const CBlockIndex *pindexFork;
         bool fInitialDownload;
+        int nNewHeight;
         while (true) {
             TRY_LOCK(cs_main, lockMain);
             if (!lockMain) {
@@ -3815,11 +3814,14 @@ bool ActivateBestChain(CValidationState& state, const CBlock* pblock, bool fAlre
             pindexNewTip = chainActive.Tip();
             pindexFork = chainActive.FindFork(pindexOldTip);
             fInitialDownload = IsInitialBlockDownload();
+            nNewHeight = chainActive.Height();
             break;
         }
 
         // When we reach this point, we switched to a new tip (stored in pindexNewTip).
         // Notifications/callbacks that can run without cs_main
+        if(connman)
+            connman->SetBestHeight(nNewHeight);
         // Always notify the UI if a new block tip was connected
         if (pindexFork != pindexNewTip) {
 
