18561b631400768effdffd00dc607e6f3bb3e196
lyricidal <lyricidal@gmail.com>
2022-09-13 12:31:02 -0700

    [Consensus] New modifier enforcement height set.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -144,7 +144,7 @@ public:
         nHardForkBlock = 375000; // Add hard fork block for Consensus/PoA Padding
         nHardForkBlockRingSize = 750000; // Add hard fork block for Ring Size bump
         nHardForkBlockRingSize2 = nHardForkBlockRingSize; // For testnet & compile purposes
-        nBlockStakeModifierlV2 = 1000000; // !TODO: change me (and add others for Testnet and RegTest)
+        nBlockStakeModifierlV2 = 1000000;
 
         /**
          * Build the genesis block. Note that the output of the genesis coinbase cannot
@@ -302,6 +302,7 @@ public:
         nHardForkBlock = 700; // Add hard fork block for Consensus/PoA Padding
         nHardForkBlockRingSize = 16000; // Add hard fork block for Ring Size bump to 25-30
         nHardForkBlockRingSize2 = 126000; // Add hard fork block for Ring Size bump to 30-32
+        nBlockStakeModifierlV2 = 135000;
 
         //! Modify the testnet genesis block so the timestamp is valid for a later start.
         genesis.nTime = 1608422400;
@@ -408,6 +409,7 @@ public:
         nStakeMinDepth = 0;
         nMasternodeCountDrift = 4;
         nModifierUpdateBlock = 0; //approx Mon, 17 Apr 2017 04:00:00 GMT
+        nBlockStakeModifierlV2 = -1;
 
         //! Modify the regtest genesis block so the timestamp is valid for a later start.
         genesis.nTime = 1608422399;
