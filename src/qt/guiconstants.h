e101ff91542c5c475321601d69627c2d9d960d28
lyricidal <lyricidal@gmail.com>
2022-10-12 10:26:55 -0400

    [Qt] Move SINGLE_THREAD_MAX_TXES_SIZE / MAX_AMOUNT_LOADED_RECORDS to guiconstants
    To be used on our History tab

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -19,6 +19,13 @@ static const int STATUSBAR_ICONSIZE = 16;
 
 static const bool DEFAULT_SPLASHSCREEN = true;
 
+#define SINGLE_THREAD_MAX_TXES_SIZE 4000
+
+// Maximum amount of loaded records in ram in the first load.
+// If the user has more and want to load them:
+// TODO, add load on demand in pages (not every tx loaded all the time into the records list).
+#define MAX_AMOUNT_LOADED_RECORDS 20000
+
 /* Invalid field background style */
 #define STYLE_INVALID "background:#FF8080"
 
